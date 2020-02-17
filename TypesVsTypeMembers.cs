2bbea0c29ac7f842c8515cd1d20a576acd8b6dea
Pujan Bajra <pujanisme@gmail.com>
2020-02-17 17:53:32 +0545

    part 48 don

---
+++ TypesVsTypeMembers.cs
@@ -0,0 +1,11 @@
+// Classes, structs, enums, interfaces, delegates are called as types and 
+// fields, properties, constructors , methods etc. that are in a type are called as type members.
+
+// There are five different access modifiers
+// Private
+// Protected
+// Internal
+// Protected Internal
+// Public
+
+// Type members can have all the access modifiers whereas types can only have 2 access modifiers (public and internal)
